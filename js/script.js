65fd480135d5a4d2bfca85bbf0e3795478ee594d
concalve911 <aleksandrgarkusha911@gmail.com>
2024-07-12 17:56:27 +0200

    changes2

--- js/script.js
+++ js/script.js
@@ -24,18 +24,18 @@ if (surName === null) {
 const selectAge = prompt("Enter your age");
 let age = null;
 if (selectAge === null) {
-  // Проверяем, не отменил ли пользователь запрос
+  // проверяем не отменил ли пользователь запрос
   alert("You Canceled");
 } else {
-  age = Number(selectAge); // Преобразуем строку в число
+  age = Number(selectAge); // преобразуем строку в число
   if (isNaN(age)) {
-    // Проверяем, является ли введенные данные числом
+    // Проверяем является ли введенные данные числом
     alert("You entered a wrong numberad");
   } else if (age < 0) {
-    // Проверяем, не является ли число негативным
+    // проверяем не является ли число негативным
     alert("You entered a wrong number");
   } else if (age > 150) {
-    // Проверяем, не превышает ли возраст допустимый диапазон
+    // проверяем не превышает ли возраст допустимый диапазон
     alert("You entered a wrong number");
   } else {
     alert(`Your age is ${age}`);
