df8e7d024e6364bafa2c326e9a0214bbbacf717d
concalve911 <aleksandrgarkusha911@gmail.com>
2024-07-12 07:32:22 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,6 @@
+"use strict";
+const name = prompt("Enter your name");
+const surname = prompt("Enter your surname");
+const age = prompt("Enter your age");
+const result = `Your name is ${name} , your surname is ${surname}, your age is ${age}`;
+console.log(result);
